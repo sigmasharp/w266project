8ed8074c0395aa25d1108492e0633efa1404a231
sigmasharp <yjawang@gmail.com>
2016-12-17 21:38:43 -0800

    finished rnn/nyt trend

--- rnn/nyt-trend/utils.py
+++ rnn/nyt-trend/utils.py
@@ -3,10 +3,13 @@ import time
 import itertools
 import numpy as np
 import pandas as pd
+import nltk
+import vocabulary
+from nltk.tokenize import word_tokenize
+import unicodedata
 from IPython.display import display
 
 def flatten(list_of_lists):
-    """Flatten a list-of-lists into a single list."""
     return list(itertools.chain.from_iterable(list_of_lists))
 
 def pretty_print_matrix(M, rows=None, cols=None, dtype=float):
@@ -42,85 +45,88 @@ def canonicalize_words(words, **kw):
 
 ##
 # Data loading functions
-import nltk
-import vocabulary
-from nltk.tokenize import word_tokenize
-import unicodedata
 
-def get_sents(name="brown"):
-    if name=='brown':
-        return nltk.corpus.__getattr__(name)
-
-    return [word_tokenize(unicode(s.decode('utf-8').strip())) for s in open('./'+name)]
-
+def get_sents(): 
+    return [word_tokenize(unicode(s.decode('utf-8').strip())) for s in open('./text.full.txt')],[word_tokenize(s.strip()) for s in open('./sn0p.full.txt')]
+                          
 def build_vocab(sents, V=10000):
     words = flatten(sents)
     token_feed = (canonicalize_word(w) for w in words)
-    vocab = vocabulary.Vocabulary(token_feed, size=V)
+    vocab = vocabulary.Vocabulary(False, token_feed, size=V)
     return vocab
 
-def get_train_test_sents(sents, split=0.8, shuffle=False):
-    """Get train and test sentences."""
+def get_train_test_dev_sents(sents, sentis, train=0.5, test=0.25, shuffle=True):
+    """Get train, test, and dev sentences."""
     sentences = np.array(sents, dtype=object)
     fmt = (len(sentences), sum(map(len, sentences)))
     print "Loaded %d sentences (%g tokens)" % fmt
 
+    sentiments = np.array(sentis, dtype=object)
+    fmt = (len(sentiments), sum(map(len, sentiments)))
+    print "Loaded %d sentiments (%g tokens)" % fmt
+
     if shuffle:
-        rng = np.random.RandomState(shuffle)
-        rng.shuffle(sentences)  # in-place
-    split_idx = int(split * len(sentences))
-    train_sentences = sentences[:split_idx]
-    test_sentences = sentences[split_idx:]
+        #rng = np.random.RandomState(shuffle)
+        shuf_idx = np.random.permutation(len(sentences))
+        sentences = sentences[shuf_idx]
+        sentiments = sentiments[shuf_idx]
+    split_train_idx = int(train * len(sentences))
+    split_test_idx = int((train + test) * len(sentences))
+    train_sents = sentences[:split_train_idx]
+    test_sents = sentences[split_train_idx:split_test_idx]
+    dev_sents = sentences[split_test_idx:]
 
-    fmt = (len(train_sentences), sum(map(len, train_sentences)))
+    fmt = (len(train_sents), sum(map(len, train_sents)))
     print "Training set: %d sentences (%d tokens)" % fmt
-    fmt = (len(test_sentences), sum(map(len, test_sentences)))
+    fmt = (len(test_sents), sum(map(len, test_sents)))
     print "Test set: %d sentences (%d tokens)" % fmt
+    fmt = (len(dev_sents), sum(map(len, dev_sents)))
+    print "dev set: %d sentences (%d tokens)" % fmt
 
-    return train_sentences, test_sentences
+    train_sentis = sentiments[:split_train_idx]
+    test_sentis = sentiments[split_train_idx:split_test_idx]
+    dev_sentis = sentiments[split_test_idx:]
 
-def preprocess_sentences(sentences, vocab):
+    fmt = (len(train_sentis), sum(map(len, train_sentis)))
+    print "Training set: %d sentiments (%d tokens)" % fmt
+    fmt = (len(test_sentis), sum(map(len, test_sentis)))
+    print "Test set: %d sentiments (%d tokens)" % fmt
+    fmt = (len(dev_sentis), sum(map(len, dev_sentis)))
+    print "dev set: %d sentiments (%d tokens)" % fmt
+    
+    return train_sents, test_sents, dev_sents, train_sentis, test_sentis, dev_sentis, test_sents, test_sentis
+
+def preprocess_sent_sentis(sents, sentis, vocab, svocab):
     # Add sentence boundaries, canonicalize, and handle unknowns
-    words = ["<s>"] + flatten(s + ["<s>"] for s in sentences)
+    words = ["<s>"] + flatten(s + ["<s>"] for s in sents)
     words = [canonicalize_word(w, wordset=vocab.word_to_id)
              for w in words]
-    return np.array(vocab.words_to_ids(words))
-
-def preprocess_ssentences(sentences, ssentences, vocab):
-    #build corresponding sentiments
-    # Add sentence boundaries, canonicalize, and handle unknowns
-    #words = ["<s>"] + flatten(s + ["<s>"] for s in sentences)
-    #words = [canonicalize_word(w, wordset=vocab.word_to_id)
-    #         for w in words]
-    idx = 0
-    words = [int(ssentences[idx][0])]
-    for s in sentences:
-        ss = int(ssentences[idx][0])
-        for w in s:
-            words.append(ss)
-        words.append(ss)
-        idx = idx + 1
     
-    #print vocab.ordered_words()
-    return np.array(vocab.words_to_ids(words))
+    idx = 0
+    swords = [sentis[idx][0]]  #ref to first <s> at the very beginning
+    for s in sents:
+        next_sword = sentis[idx][0]
+        for w in s:
+            swords.append(next_sword) #ref to all words in a sent
+        swords.append(next_sword)     #ref to the end of each sent
+        idx = idx + 1
+                          
+    return np.array(vocab.words_to_ids(words)), np.array(svocab.words_to_ids(swords))
 
 ##
 # Use this function
-def load_corpus(name, sname, split=0.8, V=10000, Z=63, shuffle=False):
+def load_data(name, sname, train=0.5, test=0.25, V=10000, Z=8, shuffle=True):
     """Load a named corpus and split train/test along sentences."""
-    sents = get_sents(name)   
+    sents, sentis = get_sents()   
     vocab = build_vocab(sents, V)
-    ssents = get_sents(sname)
-    svocab = vocabulary.SVocabulary()
-    #print svocab.ordered_words()
-    #print svocab.words_to_ids(svocab.ordered_words())
-    train_sentences, test_sentences = get_train_test_sents(sents, split, shuffle)
-    train_ids = preprocess_sentences(train_sentences, vocab)
-    test_ids = preprocess_sentences(test_sentences, vocab)
-    train_ssentences, test_ssentences = get_train_test_sents(ssents, split, shuffle)
-    train_sids = preprocess_ssentences(train_sentences, train_ssentences, svocab)
-    test_sids = preprocess_ssentences(test_sentences, test_ssentences, svocab)
-    return vocab, train_ids, train_sids, test_ids, test_sids
+    svocab = vocabulary.Vocabulary(True)
+    
+    train_sents, test_sents, dev_sents, train_sentis, test_sentis, dev_sentis, test_sents_o, test_sentis_o = get_train_test_dev_sents(sents, sentis, train, test, shuffle)
+    train_ids, train_sids = preprocess_sent_sentis(train_sents, train_sentis, vocab, svocab)
+    test_ids, test_sids = preprocess_sent_sentis(test_sents, test_sentis, vocab, svocab)
+    dev_ids, dev_sids = preprocess_sent_sentis(dev_sents, dev_sentis, vocab, svocab)
+
+    return vocab, svocab, train_ids, train_sids, test_ids, test_sids, dev_ids, dev_sids, test_sents_o, test_sentis_o
 
 ##
 # Use this function
@@ -130,7 +136,7 @@ def batch_generator(ids, sids, batch_size, max_time):
     clip_len = ((len(ids)-1) / batch_size) * batch_size
     input_w = ids[:clip_len]     # current word
     target_y = sids[:clip_len]  # the sents
-    #print 'in batch_generator', len(ids), len(sids), len(input_w), len(target_y), clip_len, batch_size
+    print 'in batch_generator', len(ids), len(sids), len(input_w), len(target_y), clip_len, batch_size
     # Reshape so we can select columns
     input_w = input_w.reshape([batch_size,-1])
     target_y = target_y.reshape([batch_size,-1])
--- rnn/nyt-trend/vocabulary.py
+++ rnn/nyt-trend/vocabulary.py
@@ -6,27 +6,33 @@ class Vocabulary(object):
   END_TOKEN = "</s>"
   UNK_TOKEN = "<unk>"
 
-  def __init__(self, tokens, size=None):
-    self.unigram_counts = collections.Counter(tokens)
-    # leave space for "<s>", "</s>", and "<unk>"
-    top_counts = self.unigram_counts.most_common(None if size is None else (size - 3))
-    vocab = ([self.START_TOKEN, self.END_TOKEN, self.UNK_TOKEN] +
-             [w for w,c in top_counts])
+  def __init__(self, senti_vocab=False, tokens=None, size=None):
+    self.is_senti = senti_vocab
+    if not senti_vocab:
+        self.unigram_counts = collections.Counter(tokens)
+        # leave space for "<s>", "</s>", and "<unk>"
+        top_counts = self.unigram_counts.most_common(None if size is None else (size - 3))
+        vocab = ([self.START_TOKEN, self.END_TOKEN, self.UNK_TOKEN] +
+                 [w for w,c in top_counts])
 
+    else:
+        vocab = ('n', '0', 'p') 
+    
     # Assign an id to each word, by frequency
     self.id_to_word = dict(enumerate(vocab))
     self.word_to_id = {v:k for k,v in self.id_to_word.iteritems()}
     self.size = len(self.id_to_word)
     if size is not None:
         assert(self.size <= size)
-
-    # Store special IDs
-    self.START_ID = self.word_to_id[self.START_TOKEN]
-    self.END_ID = self.word_to_id[self.END_TOKEN]
-    self.UNK_ID = self.word_to_id[self.UNK_TOKEN]
+        
+    if not senti_vocab:
+        # Store special IDs
+        self.START_ID = self.word_to_id[self.START_TOKEN]
+        self.END_ID = self.word_to_id[self.END_TOKEN]
+        self.UNK_ID = self.word_to_id[self.UNK_TOKEN]
 
   def words_to_ids(self, words):
-    return [self.word_to_id.get(w, self.UNK_ID) for w in words]
+    return [self.word_to_id.get(w, 1 if self.is_senti else self.UNK_ID) for w in words]
 
   def ids_to_words(self, ids):
     return [self.id_to_word[i] for i in ids]
@@ -37,29 +43,3 @@ class Vocabulary(object):
   def ordered_words(self):
     """Return a list of words, ordered by id."""
     return self.ids_to_words(range(self.size))
-
-class SVocabulary(object):
-
-  def __init__(self, size=None):
-    vocab = (1, 3, 5, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 59, 61, 62, 63, 70, 71, 75, 76, 78, 81)
-
-    # Assign an id to each word, by frequency
-    self.id_to_word = dict(enumerate(vocab))
-    self.word_to_id = {v:k for k,v in self.id_to_word.iteritems()}
-    self.size = len(self.id_to_word)
-    if size is not None:
-        assert(self.size <= size)
-
-  def words_to_ids(self, words):
-    return [self.word_to_id.get(w, 0) for w in words]
-
-  def ids_to_words(self, ids):
-    return [self.id_to_word[i] for i in ids]
-
-  def sentence_to_ids(self, words):
-    return self.words_to_ids(words)
-
-  def ordered_words(self):
-    """Return a list of words, ordered by id."""
-    return self.ids_to_words(range(self.size))
-
